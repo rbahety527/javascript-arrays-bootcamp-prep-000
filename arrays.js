cc9427494d9ba43b02e818833ea506bcdf0dd041
Ruchi Bahety <ruchi.malpani@gmail.com>
2018-04-11 06:39:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,33 +1,7 @@
-var ing1 = "bread"
-var ing2 = "mild cheese"
-var ing3 = "sharp cheese"
-var ing4 = "butter"
-var ing5 = "tomato"
-var ing6 = "garlic"
-
-var grilledCheeseIngredients = 
+var chocolateBars = 
 [
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic'
-  ]
-  
-var tomatoSauceIngredients = 
-[
-  'tomato',
-  'garlic',
-  'olive oil',
-  'basil',
-  'oregano'
-  ]
-  
-  var chocolateBars = 
-  [
-    'snickers',
-    'hundred grand',
-    'kitkat',
-    'skittles'
-    ]
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
